3d90161c94eb65b356dde25c5512ea29015f43e5
knon <knon@prolly.net>
2017-01-29 17:04:01 +0100

    Moved collection management into CompositeRenderable.

--- slick2d-core/src/main/java/org/newdawn/slick/CompositeRenderable.java
+++ slick2d-core/src/main/java/org/newdawn/slick/CompositeRenderable.java
@@ -1,21 +1,26 @@
 package org.newdawn.slick;
 
 import com.sun.javafx.geom.Vec2f;
-import java.util.HashMap;
+import java.util.LinkedHashMap;
 import java.util.Map;
 
 /**
  * A {@link Renderable} that is composed of multiple other {@link Renderable}
  * instances.
+ * <p>
+ * Note that the {@link Renderable}s get rendered in the order that they are
+ * added.
  */
 public class CompositeRenderable implements Renderable {
 
   private final Map<Vec2f, Renderable> renderables;
 
-  public CompositeRenderable(Map<Vec2f, Renderable> renderables) {
-    this.renderables = new HashMap<>(renderables);
+  public CompositeRenderable(int nrOfImages) {
+    this.renderables = new LinkedHashMap<>(nrOfImages);
+  }
 
-
+  public void add(float offsetX, float offsetY, Renderable renderable) {
+    renderables.put(new Vec2f(offsetX, offsetY), renderable);
   }
 
   @Override
