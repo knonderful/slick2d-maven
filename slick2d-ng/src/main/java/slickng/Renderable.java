cad0db883039f663d9df802e36c51ee722e663ac
knon <knon@prolly.net>
2017-02-04 18:50:10 +0100

    Added Javadoc to Renderable.

--- slick2d-ng/src/main/java/slickng/Renderable.java
+++ slick2d-ng/src/main/java/slickng/Renderable.java
@@ -1,6 +1,14 @@
 package slickng;
 
+/**
+ * An entity that can be rendered to a {@link RenderContext}.
+ */
 public interface Renderable {
 
+  /**
+   * Render the entity.
+   *
+   * @param renderContext The {@link RenderContext}.
+   */
   void render(RenderContext renderContext);
 }
