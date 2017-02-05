aefd2454a9cd820db74f72aa4c1d684602c27107
knon <knon@prolly.net>
2017-02-05 22:57:25 +0100

    Added Javadoc to Surface.

--- slick2d-ng/src/main/java/slickng/gfx/Surface.java
+++ slick2d-ng/src/main/java/slickng/gfx/Surface.java
@@ -1,10 +1,30 @@
 package slickng.gfx;
 
+/**
+ * A two-dimensional surface that contains graphics information.
+ */
 public interface Surface {
 
+  /**
+   * Retrieves the width of the surface.
+   *
+   * @return The width in pixels.
+   */
   int getWidth();
-  
+
+  /**
+   * Retrieves the height of the surface.
+   *
+   * @return The height in pixels.
+   */
   int getHeight();
 
+  /**
+   * Creates a {@link TileSheet} for the surface.
+   *
+   * @param tileWidth  The tile width.
+   * @param tileHeight The tile height.
+   * @return The {@link TileSheet}.
+   */
   TileSheet createTileSheet(int tileWidth, int tileHeight);
 }
